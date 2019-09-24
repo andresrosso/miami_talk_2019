194fb7de83768dfb27d59529e84e0cb364d35fe4
andres rosso <andresrosso@gmail.com>
2019-09-24 23:49:24 +0000

    treceval

--- qa_tutorial.sh
+++ qa_tutorial.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
-
+echo "Download word2vec embedding"
 wget https://s3.amazonaws.com/dl4j-distribution/GoogleNews-vectors-negative300.bin.gz
-gunzip GoogleNews-vectors-negative300.bin.gz ../
+gunzip GoogleNews-vectors-negative300.bin.gz
+mv GoogleNews-vectors-negative300.bin ../
+
+echo "Install trec_eval information retrieval evaluation tool"
+tar -xvf trec_eval_latest.tar.gz
+sed -i 's/gcc/clang/g' trec_eval.9.0/Makefile
+#cat trec_eval.9.0/Makefile
+cd trec_eval.9.0
+make
+cd ..
